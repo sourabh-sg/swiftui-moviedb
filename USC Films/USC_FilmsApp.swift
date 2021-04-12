81b28072711d2559c2ed73f00391ce2604afe1b9
sourabh-sg <61243367+sourabh-sg@users.noreply.github.com>
2021-04-12 22:27:14 +0530

    Initial Commit

---
+++ USC Films/USC_FilmsApp.swift
@@ -0,0 +1,17 @@
+//
+//  USC_FilmsApp.swift
+//  USC Films
+//
+//  Created by Sourabh Gapate on 12/04/21.
+//
+
+import SwiftUI
+
+@main
+struct USC_FilmsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
